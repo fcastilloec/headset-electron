555411002ba3f1be5fea7cb301163e9f4ff970fe
Felipe Castillo <fcastillo.ec@gmail.com>
2020-04-07 22:14:13 -0700

    fix getting tags

--- bin/getTags.js
+++ bin/getTags.js
@@ -1,10 +1,14 @@
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 
-async function main () {
+async function main() {
   try {
-    const { stdout: oldtag, stderr: err1 } = await exec('git describe --abbrev=0 --tags `git rev-list --tags --skip=1 --max-count=1`');
-    const { stdout: newtag, stderr: err2 } = await exec('git describe --abbrev=0 --tags `git rev-list --tags --max-count=1`');
+    const { stdout: oldtag } = await exec(
+      'git describe --abbrev=0 --tags `git rev-list --tags --skip=1 --max-count=1`',
+    );
+    const { stdout: newtag } = await exec(
+      'git describe --abbrev=0 --tags `git rev-list --tags --max-count=1`',
+    );
 
     console.log('\x1b[32m%s\x1b[0m', `Previous tag: ${oldtag}`);
     console.log('\x1b[32m%s\x1b[0m', `Latest tag: ${newtag}`);
@@ -12,9 +16,9 @@ async function main () {
     console.log(`::set-output name=newtag::${newtag}`);
   } catch (error) {
     console.log('\x1b[33m%s\x1b[0m', 'No tags found: ');
-    console.log('\x1b[31m%s\x1b[0m', `${err1}\n${err2}`);
+    console.log('\x1b[31m%s\x1b[0m', error.stderr);
     process.exit(1);
   }
 }
 
-main()
+main();
